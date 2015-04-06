1875c274797e776b5b2bb2aa357c49e37eff932e
shannon <shannonjc143@gmail.com>
2015-04-06 15:54:55 -0500

    Shuffle exercise added

---
+++ Rails_Tutorial_Exercises/Rails_Tutorial_Exercises.rb
@@ -0,0 +1,10 @@
+#By replacing the question marks in Listing 4.14
+ # with the appropriate methods, combine split, shuffle, 
+ # and join to write a function that shuffles the letters
+ #  in a given string. 
+
+def string_shuffle(s)
+   s.split('').shuffle.join
+end
+
+
