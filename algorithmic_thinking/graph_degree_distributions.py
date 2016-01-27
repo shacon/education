1a641656231af7f7c86cf9d9f327441c7900c0c6
shannon <shannonjc143@gmail.com>
2016-01-27 12:00:55 -0600

    add in_degree distribution

--- algorithmic_thinking/graph_degree_distributions.py
+++ algorithmic_thinking/graph_degree_distributions.py
@@ -4,7 +4,7 @@ Python code that creates dictionaries corresponding to some simple examples of g
 along with two short functions that compute information
 about the distribution of the in-degrees for nodes in these graphs
 """
-
+#sample graphs
 EX_GRAPH0 = {0:set([1, 2]),1:set([]), 2:set([])}
 EX_GRAPH1 = {0:set([1, 4, 5]),1:set([2, 6]),2:set([3]),3:set([0]),
              4:set([1]),5:set([2]),6:set([])}
@@ -24,18 +24,55 @@ def make_complete_graph(num_nodes):
         counter += 1
     return node_dict
 
+def compute_in_degrees(digraph):
+    #Takes a directed graph digraph (represented as a dictionary) and computes
+    #the in-degrees for the nodes in the graph. The function should return a dictionary with
+    #the same set of keys (nodes) as digraph
+    #whose corresponding values are the number of edges whose head matches a particular node.
 
+    #create a list of the nodes that appear as values in the given digraph
+    node_list = generate_head_nodes_list(digraph)
+    #count the number of times each number appears in the list
+    degrees = dict.fromkeys(digraph)
+    for key, val in degrees.items():
+        degrees[key] = node_list.count(key)
+    return degrees
 
+def generate_head_nodes_list(digraph):
+    edges_head_list = []
+    for key, val in digraph.items():
+        if val:
+           for item in val:
+               edges_head_list.append(item)
+    return edges_head_list
 
 
 class TestGraphMethods(unittest.TestCase):
 
+
   def test_make_complete_graph(self):
       self.assertEqual(make_complete_graph(1), {0:set([])})
       self.assertEqual(make_complete_graph(2), {0:set([1]), 1:set([0])})
+      self.assertEqual(make_complete_graph(4), {0:set([1, 2, 3]), 1:set([0, 2, 3]),
+        2:set([0, 1, 3]), 3:set([0, 1, 2])})
+
+  def test_gen_head_nodes(self):
+      GRAPH0 = {0: set([1]),
+          1: set([2]),
+          2: set([3]),
+          3: set([0])}
+      self.assertEqual(generate_head_nodes_list(EX_GRAPH0), [1, 2])
+      self.assertEqual(generate_head_nodes_list(GRAPH0), [1, 2, 3, 0])
 
 
-
+  def test_compute_in_degree(self):
+      # digraph = {}
+      # self.assertEqual(compute_in_degrees(digraph), )
+      GRAPH0 = {0: set([1]),
+          1: set([2]),
+          2: set([3]),
+          3: set([0])}
+      self.assertEqual(compute_in_degrees(GRAPH0), {0: 1, 1: 1, 2: 1, 3: 1})
 
 if __name__ == '__main__':
     unittest.main()
